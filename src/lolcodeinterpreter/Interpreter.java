dfa53606acb02e5d6f947fbefcd0e51b46b6d9d7
Kristine Labador <knlabador@up.edu.ph>
2020-11-30 23:51:22 +0800

    adds ! token for visible

--- src/lolcodeinterpreter/Interpreter.java
+++ src/lolcodeinterpreter/Interpreter.java
@@ -2,7 +2,6 @@ package lolcodeinterpreter;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Map.Entry;
